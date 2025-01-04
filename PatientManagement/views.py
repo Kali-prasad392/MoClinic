0725febc570770b6850fb0c42a089faab22c6370
shamaun.alam <shamaun.alam@secuodsoft.com>
2025-01-04 16:51:11 +0530

    shamaun updated function template in views

--- PatientManagement/views.py
+++ PatientManagement/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def register_patient(request):
+    pass
+
+def enqueue_patient(request):
+    pass
+
+def dequeue_patient(request):
+    pass
+
+def call_patient(request):
+    pass
+
+def get_patient_details(request):
+    pass
+
+def prescribe(request):
+    pass
--- UserAndAccessManagement/views.py
+++ UserAndAccessManagement/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from .models import CustomUser
+from django.contrib.auth import authenticate,login,logout
 # Create your views here.
+
+def signin(request):
+    pass
+
+
